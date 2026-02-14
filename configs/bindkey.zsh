bb8538b47d9d923d45c84c47ea4c3e157a2a8764
Yuya Makiyama <yuya.makiyama@gmail.com>
2026-02-13 19:10:59 -0500

    Update bindkey.zsh to support newline insertion for Ghostty, iTerm2, and VSCode keybindings

--- configs/bindkey.zsh
+++ configs/bindkey.zsh
@@ -1,6 +1,8 @@
-# Fix Shift+Enter to insert a newline in Ghostty
 function insert-newline() {
   LBUFFER+=$'\n'
 }
 zle -N insert-newline
-bindkey ';2;13~' insert-newline
+# Ghostty / iTerm2
+bindkey '\e[27;2;13~' insert-newline
+# Cursor / VSCode (via keybindings.json sendSequence)
+bindkey '\e[13;2u' insert-newline
